6c95ff50d27fb26ed197868280b24e616175b085
Andrei Punko <Andrei_Punko@epam.com>
2020-05-27 13:01:34 +0300

    Add generated project template

---
+++ src/test/java/by/andd3dfx/springboottemplate/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package by.andd3dfx.springboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
