f5940eff8d8e14c64e1a9ca86f5b8d29994a196c
Andrei Punko <andd3dfx@gmail.com>
2020-12-15 19:27:20 +0300

    Replace testing vs H2 in-memory DB with testcontainers approach

---
+++ src/test/java/by/andd3dfx/templateapp/IntegrationTestInitializer.java
@@ -0,0 +1,41 @@
+package by.andd3dfx.templateapp;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    private static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:9.6.20");
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        startContainers();
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+
+        MapPropertySource propertySource = new MapPropertySource(
+                "test-containers", (Map) createConnectionConfiguration()
+        );
+        environment.getPropertySources().addFirst(propertySource);
+    }
+
+    private static void startContainers() {
+        Startables.deepStart(Stream.of(postgres)).join();
+        // we can add further containers
+        // here like rabbitmq or other databases
+    }
+
+    private static Map<String, String> createConnectionConfiguration() {
+        return Map.of(
+                "DB_URL", postgres.getJdbcUrl(),
+                "DB_USERNAME", postgres.getUsername(),
+                "DB_PASSWORD", postgres.getPassword()
+        );
+    }
+}
--- src/test/java/by/andd3dfx/templateapp/SpringBootTemplateApplicationTests.java
+++ src/test/java/by/andd3dfx/templateapp/SpringBootTemplateApplicationTests.java
@@ -2,12 +2,13 @@ package by.andd3dfx.templateapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(initializers = IntegrationTestInitializer.class)
 @SpringBootTest
 class SpringBootTemplateApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
