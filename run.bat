82b94c1e44e3e4013f672f073abcfa4c2c7407e3
Andrei Punko <andd3dfx@gmail.com>
2021-03-16 16:18:52 +0300

    Adjust README and starting script

--- run.bat
+++ run.bat
@@ -4,4 +4,5 @@ java -jar target/spring-boot-template-0.0.1-SNAPSHOT.jar ^
  --spring.datasource.url=jdbc:h2:mem:testdb ^
  --spring.datasource.username=sa ^
  --spring.datasource.password=password ^
- --spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect
+ --spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect ^
+ --spring.datasource.driver-class-name=org.h2.Driver
