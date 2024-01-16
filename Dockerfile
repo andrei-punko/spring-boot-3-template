c4fb619ac72f26e63694476df71d37b4bfca9766
Andrei Punko <andd3dfx@gmail.com>
2024-01-16 22:27:02 +0300

    Change artifact name: spring-boot-template -> spring-boot-3-template

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:17-jdk-slim
 EXPOSE 9080
-ADD target/spring-boot-template-0.0.1-SNAPSHOT.jar /usr/local/app.jar
+ADD target/spring-boot-3-template-0.0.1-SNAPSHOT.jar /usr/local/app.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Dspring.profiles.active=container", "-jar", "/usr/local/app.jar"]
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 echo You should build jar before running this script. See README for details
 
-java -jar target/spring-boot-template-0.0.1-SNAPSHOT.jar ^
+java -jar target/spring-boot-3-template-0.0.1-SNAPSHOT.jar ^
  --spring.datasource.url=jdbc:h2:mem:testdb ^
  --spring.datasource.username=sa ^
  --spring.datasource.password=password ^
