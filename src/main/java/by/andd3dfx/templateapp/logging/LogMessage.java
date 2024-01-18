9f471f78679546cfdb1f1d0f2c41ea0863a2fbda
Andrei Punko <andd3dfx@gmail.com>
2024-01-18 13:30:33 +0300

    Log HTTP requests & responses. Fix #20

---
+++ src/main/java/by/andd3dfx/templateapp/logging/LogMessage.java
@@ -0,0 +1,24 @@
+package by.andd3dfx.templateapp.logging;
+
+import lombok.Data;
+
+@Data
+public class LogMessage {
+
+    private int httpStatus;
+    private String httpMethod;
+    private String path;
+    private String clientIp;
+    private String javaMethod;
+    private String response;
+
+    @Override
+    public String toString() {
+        return httpStatus + " " +
+                httpMethod + " " +
+                "'" + path + '\'' +
+                ", clientIp='" + clientIp + '\'' +
+                //", javaMethod='" + javaMethod + '\'' +
+                ", response='" + response + '\'';
+    }
+}
---
+++ src/main/java/by/andd3dfx/templateapp/logging/RequestResponseLoggingConfig.java
@@ -0,0 +1,21 @@
+package by.andd3dfx.templateapp.logging;
+
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class RequestResponseLoggingConfig {
+
+    @Bean
+    public ServletRegistrationBean dispatcherRegistration() {
+        return new ServletRegistrationBean(dispatcherServlet());
+    }
+
+    @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+    public DispatcherServlet dispatcherServlet() {
+        return new LoggableDispatcherServlet();
+    }
+}
