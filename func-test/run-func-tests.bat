dd15e093fd6161bce3d0f0b188708fb6606ff644
Andrei Punko <andd3dfx@gmail.com>
2024-06-08 16:59:46 +0300

    Move script to run func-tests into root folder

--- func-test/run-func-tests.bat
+++
@@ -1,3 +0,0 @@
-echo "Run functional tests. Check reports at ./build/spock-reports/index.html"
-
-./gradlew clean build
---
+++ run-func-tests.bat
@@ -0,0 +1,5 @@
+
+echo "Run functional tests. Check reports at ./func-test/build/spock-reports/index.html"
+
+cd func-test
+./gradlew clean build
